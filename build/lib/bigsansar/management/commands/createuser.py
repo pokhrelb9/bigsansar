ddb8ee5de0e2ad34dca3581df06ea7e5653c1107
Bikash Pokhrel <pokhrelb9@gmail.com>
2022-07-02 14:52:08 +0900

    0.0.6

--- build/lib/bigsansar/management/commands/createuser.py
+++ build/lib/bigsansar/management/commands/createuser.py
@@ -1,34 +1,44 @@
+from getpass import getpass
+
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
 
 
 class Command(BaseCommand):
     help = "Closes the specified poll for voting"
 
     def handle(self, *args, **options):
-        print('We are creating 1000 fake user..............')
-
+        print('We are creating User..............')
 
         print('please wait a minute.................')
 
+        print('How many user you want to create?')
+        numbers = int(input())
 
+        print('wait..........')
+        print(''
+              '')
 
-        for _ in range(1000):
+        print('Put your password here')
+        password = getpass()
+
+        for _ in range(numbers):
             try:
 
-                get_uname = 'fake_'
+                get_uname = 'bp'
                 total_user = User.objects.last().id + 1
                 username = get_uname + str(total_user)
-
 
             except:
                 break
 
             else:
 
-                create = User.objects.create_user(username=username, password='bp787898')
+                create = User.objects.create_user(username=username, password=password)
 
                 print(create, '........ ok')
 
+            finally:
+                print('User created successfully')
+                print('Finished')
 
