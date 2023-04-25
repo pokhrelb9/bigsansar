e9d15166dad271d50fef7786ecb02791fd0da624
Bikash Pokhrel <101688346+pokhrelb9@users.noreply.github.com>
2023-04-25 22:13:15 +0900

    Delete Bigsansar-0.9.9 directory

--- Bigsansar-0.9.9/bigsansar/contrib/blogs/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bigsansar.contrib.blogs'
-    verbose_name_ = 'blogs'
--- Bigsansar-0.9.9/bigsansar/main.py
+++
@@ -1,2 +0,0 @@
-def int(request):
-    pass
