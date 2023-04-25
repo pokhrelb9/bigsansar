5d78ef30f328ed463041f0f391007b02b8a1f18f
Bikash Pokhrel <101688346+pokhrelb9@users.noreply.github.com>
2023-04-25 22:09:47 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Bigsansar",
+    version="0.9.9",
+    author="Bikash Pokhrel",
+    author_email="bigsansaroffice@gmail.com",
+    description="Build one in minutes with bigsansar - a visual site building tool!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bigsansar.com",
+    project_urls={
+        "Bug Tracker": "https://github.com/pokhrelb9/bigsansar/issues",
+        "Documentations": "https://docs.bigsansar.com/"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    packages=setuptools.find_packages(),
+    package_data={'': ['templates/*', 'static/*']},
+
+    entry_points={
+        'console_scripts': [
+                    'bigsansar = bigsansar.core:main',
+                ],
+    },
+
+    python_requires=">=3.6",
+    install_requires=['django', 'django-phonenumber-field[phonenumberslite]', 'django-ckeditor', 'requests', 'pillow'],
+    keywords=['python', 'django host', 'bigsansar', 'django', 'django sites framework', 'django flatpages']
+)
