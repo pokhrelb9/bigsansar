75d9c7647dd73228c8470264a702656caafa553c
pokhrelb9 <pokhrelb9@gmail.com>
2023-04-29 18:25:23 +0900

    1.1.2

--- www/settings.py
+++ www/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u#r$$=-#khumewdf@*v$@9^-r01d$vx*ev3s-x#fn1#%d1x=+#'
+SECRET_KEY = 'django-insecure-xsv%gtm-f%qw6&)dgovo^5^2%pqd@$bh#*b7xqlu1(4jagj((9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
